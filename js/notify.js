f596727807fa2254819b5570af4f1a58391a9b8b
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-12-26 00:48:03 +0530

    Add timeout delay logic in notify.js

---
+++ js/notify.js
@@ -0,0 +1,14 @@
+const MICROVERSE_TIME = -6; // +1 or -6
+const EVENING_MEETING_HOUR = 17;
+
+const offsetHour = Math.floor(Math.abs((new Date().getTimezoneOffset() / 60) + (MICROVERSE_TIME)));
+
+let localHour = EVENING_MEETING_HOUR + offsetHour;
+if (localHour > 24) localHour -= 24;
+
+const localMinutes = Math.floor(Math.abs(new Date().getTimezoneOffset() % 60));
+
+const timeoutDelay = new Date(`${new Date().toDateString()} ${localHour}:${localMinutes}:00`).getTime() - new Date().getTime();
+// const timeoutDelay = new Date(new Date().toDateString() + ` 00:43:00`).getTime() - new Date().getTime();
+
+// console.log("Notification coming in", (new Date(new Date().toDateString() + ` ${localHour}:${localMinutes}:00`).getTime() - new Date().getTime()) / 1000 / 60 / 60);
